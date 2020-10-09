be1f3c4e44e45c68dfa3eeeae0d457a9c2851791
Matin Mirzaei <45024401+MatinMirzaei@users.noreply.github.com>
2020-10-09 11:03:20 -0700

    added imports and TextView

--- app/src/main/java/com/example/app1/MainActivity.java
+++ app/src/main/java/com/example/app1/MainActivity.java
@@ -2,13 +2,24 @@ package com.example.app1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private Intent activityIntent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    }
+
+    public void sayHello(View view) {
+        Log.i("INFO", "Hello World!");
+        TextView tv = findViewById(R.id.ViewText);
     }
 }

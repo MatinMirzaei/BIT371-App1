7a403aab13003071127a1f00791055facab4b1f1
Matin Mirzaei <45024401+MatinMirzaei@users.noreply.github.com>
2020-10-15 20:27:39 -0700

    Built an array with list of cities in it

--- app/src/main/java/com/example/app1/MainActivity.java
+++ app/src/main/java/com/example/app1/MainActivity.java
@@ -6,37 +6,28 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.ListView;
 
 
 public class MainActivity extends AppCompatActivity {
-    private Intent activityIntent;
+
+    private final String[] cities = {"Seattle", "Bothell", "Kirkland", "Bellevue", "Lynnwood", "Renton", "Redmond",
+            "Spokane", "Vancouver", "Tacoma", "Olympia", "Bellingham", "Arlington", "Everett", "Woodinville",
+            "Monroe", "New Castle", "Ballard", "Lacey"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
 
-
-
-    public void goToActivity(View view) {
-        Log.i("INFO","Logging in to " + UserConfirmationActivity.class.getName());
-        EditText user = findViewById(R.id.username);
-        EditText pass = findViewById(R.id.password);
-        String fixusername = user.getText().toString();
-        String fixpassword = pass.getText().toString();
-
-
-        Intent intent = new Intent (getApplicationContext(),UserConfirmationActivity.class);
-        intent.putExtra("USERNAME", fixusername);
-        intent.putExtra("PASSWORD", fixpassword);
-        startActivity(intent);
-
-
-
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, R.layout.listview_layout, R.id.cities, cities);
+        ListView tv = findViewById(R.id.city_list);
+        tv.setAdapter(adapter);
 
     }
+
 }
 
 

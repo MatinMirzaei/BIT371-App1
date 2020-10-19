c691abe2ba05435e2a479d93ae6c0040c2335092
Matin Mirzaei <45024401+MatinMirzaei@users.noreply.github.com>
2020-10-19 13:54:00 -0700

    added RecyclerView to the activity_main and LinearLayout to item_contact

--- app/src/main/java/com/example/app1/MainActivity.java
+++ app/src/main/java/com/example/app1/MainActivity.java
@@ -13,18 +13,13 @@ import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private final String[] cities = {"Seattle", "Bothell", "Kirkland", "Bellevue", "Lynnwood", "Renton", "Redmond",
-            "Spokane", "Vancouver", "Tacoma", "Olympia", "Bellingham", "Arlington", "Everett", "Woodinville",
-            "Monroe", "New Castle", "Ballard", "Lacey"};
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, R.layout.listview_layout, R.id.cities, cities);
-        ListView tv = findViewById(R.id.city_list);
-        tv.setAdapter(adapter);
 
     }
 
